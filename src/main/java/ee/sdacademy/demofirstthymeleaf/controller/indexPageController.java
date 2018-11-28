0667cb066094684e046774cc97b56d0134ae236f
Ryan <hiRyan@protonmail.com>
2018-11-28 20:06:35 +0200

    Tabled students looking good. Before trying out conditions.

--- src/main/java/ee/sdacademy/demofirstthymeleaf/controller/indexPageController.java
+++ src/main/java/ee/sdacademy/demofirstthymeleaf/controller/indexPageController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Ryan Alexander on 28/11/2018.
  */
@@ -21,6 +24,12 @@ public class indexPageController {
         // used the object I created, 'Person.java'
         model.addAttribute("person", new Person("Ryan", "Alexander", 70));
 
+        List<Person> persons = new ArrayList<>();
+        persons.add(new Person ("Ryan", "Alexander", 70));
+        persons.add(new Person ("Bob", "Geldof", 78));
+        persons.add(new Person ("Anna", "Kournikova", 34));
+        persons.add(new Person ("Nessa", "DiAngella", 34));
+        model.addAttribute("students", persons);
 
         return "index";
     }
